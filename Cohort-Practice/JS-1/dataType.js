8a18a2fcc70550b70f75454134aabaa3f496651f
Vikram Kumar <vsaw@gmail.com>
2026-02-17 22:59:11 +0530

    Updated index2.js

---
+++ Cohort-Practice/JS-1/dataType.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 20;
+
+let sum =a+b;
+let isEqueal = (a===b);
+let isTrue = (true && false);
+
+console.log(a);
+console.log(b);
+console.log(sum);
---
+++ Cohort-Practice/JS-1/loops.js
@@ -0,0 +1,15 @@
+let users = ["vikram","ram","shyam","hari","gopal"];
+
+// let totalUsers = users.length;
+
+
+for(let i=0; i<users.length; i++){
+    console.log(users[i]);
+} 
+
+
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// console.log(users[3]);
+// console.log(users[4]);
